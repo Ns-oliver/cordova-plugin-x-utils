0c6e8fe38477b104b9abb4b43f0256ce865da52c
huangwenzhe <huangwenzhe@zhixueyun.com>
2018-01-29 14:57:16 +0800

    update v1.1.0 :add open url(appstore)

--- src/ios/CDVUtils.h
+++ src/ios/CDVUtils.h
@@ -10,5 +10,18 @@
 
 @interface CDVUtils : CDVPlugin
 
+/*********** 换肤 ***********/
+- (void)transPeeler:(CDVInvokedUrlCommand *)command;
+
+//检测到新版本时，点确定按钮，跳转到应用商店升级
+- (void)openURLForUpdate:(CDVInvokedUrlCommand *)command;
+
+//获取皮肤样式
++ (NSDictionary *)getPeeler;
+
+//根据十六进制颜色代码返回UIColor
++ (UIColor*)colorFromColorString:(NSString*)colorString;
+
++ (void)alertTitle:(NSString *)title content:(NSString *)content showVC:(UIViewController *)vc handler1:(void(^)(UIAlertAction * action))handler1;
 
 @end
